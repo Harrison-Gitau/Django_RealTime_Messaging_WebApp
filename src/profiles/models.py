6c5186adfeb4b9d3c6b7152cebf459d162eeca1a
Harrison-Gitau <gitau.h.mwangi@gmail.com>
2019-07-01 12:14:36 +0300

    Change database to Postgres

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class profile(models.Model):
+class Profile(models.Model):
 	name = models.CharField(max_length=120, null=False)
 	description = models.TextField(default = 'description default text')
 	department = models.CharField(max_length=120, null=False)
