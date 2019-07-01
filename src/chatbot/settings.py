202a874b3b03b2c19337c4baeb78eddd0557d753
Harrison-Gitau <gitau.h.mwangi@gmail.com>
2019-07-01 15:49:08 +0300

    Add logfile ext to Procfile

--- src/chatbot/settings.py
+++ src/chatbot/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '$-t7x1=!fu#l(dz^&2h#01x=5q9dst$r@4rp)p&2aoezt11mb&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
